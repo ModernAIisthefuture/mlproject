a5fcfc0ce8b78cb22f258dcc9e8d74401f0c15b2
unknown <piyushparmarmca@gmail.com>
2025-05-08 21:43:48 +0530

    Initial deploy

--- mlproject/settings.py
+++ mlproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$-xgsl9-_bivt55hi0^z%&996m2vym%w*ph$(f+=d9&!^mvc=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
